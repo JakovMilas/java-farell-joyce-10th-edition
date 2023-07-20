54f5fdea30e59d8ce6e66a6411dbfd05c65964a7
Jakov Milas <jakov.milas@live.com>
2023-07-21 00:03:36 +0200

    Chapter 01, You Do It Files

---
+++ you-do-it/chapter01/First.java
@@ -0,0 +1,10 @@
+public class First {
+
+	public static void main(String[] args) {
+
+		System.out.println("My new and improved");
+		System.out.println("Java application");
+
+	}
+
+}
---
+++ you-do-it/chapter01/Hello2.java
@@ -0,0 +1,16 @@
+// Filename Hello2.java
+// Written by Jakov Milas
+// Written on 20.07.2023.
+
+public class Hello2 {
+
+	/* This class demonstrates the use of the println()
+	method to print the message Hello, world! */
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello, world!");
+
+	}
+
+}
