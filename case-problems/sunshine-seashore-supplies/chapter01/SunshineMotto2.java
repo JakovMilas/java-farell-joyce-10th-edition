d66ca4b65a0bba4ac2c9ba256af5b28a2df17887
Jakov Milas <jakov.milas@live.com>
2023-07-20 23:57:40 +0200

    Chapter 01, Case Problems 2b

---
+++ case-problems/sunshine-seashore-supplies/chapter01/SunshineMotto2.java
@@ -0,0 +1,11 @@
+public class SunshineMotto2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+		System.out.println("S Sunshine Seashore makes it fun in the sun. S");
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+
+	}
+
+}
