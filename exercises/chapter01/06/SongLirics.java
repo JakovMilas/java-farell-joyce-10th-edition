1942cd8ec98936e25adcbfff616f6389daa4f4ba
Jakov Milas <jakov.milas@live.com>
2023-07-20 23:49:59 +0200

    Chapter 01, exercise 06

---
+++ exercises/chapter01/06/SongLirics.java
@@ -0,0 +1,12 @@
+public class SongLirics {
+
+	public static void main(String[] args) {
+
+		System.out.println("When that fat old sun in the sky is falling");
+		System.out.println("Summer evening birds are calling");
+		System.out.println("The sound of music in my ears");
+		System.out.println("Distant bells, new mown grass smells so sweet");
+
+	}
+
+}
