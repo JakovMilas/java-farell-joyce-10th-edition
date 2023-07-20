af183f429bb042bf6f772a14997b46589e6d19a9
Jakov Milas <jakov.milas@live.com>
2023-07-20 23:52:38 +0200

    Chapter 01, exercise 10

---
+++ exercises/chapter01/10/Comments.java
@@ -0,0 +1,17 @@
+public class Comments {
+
+	public static void main(String[] args) {
+
+		// Program comments are nonexecuting statements you add to a file for documentation.
+
+		/* Program comments are nonexecuting statements
+		   you add to a file for documentation */
+
+		/** Program comments are nonexecuting statements you add to a file for documentation */
+
+		System.out.print("Program comments are nonexecuting statements");
+		System.out.println("you add to a file for documentation");
+
+	}
+
+}
