a1d7a9fa226e23329e4add0449b889910b33100e
Jakov Milas <jakov.milas@live.com>
2023-07-20 23:50:44 +0200

    Chapter 01, exercise 07

---
+++ exercises/chapter01/07/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+public class MovieQuoteInfo {
+
+	public static void main(String[] args) {
+
+		System.out.println("I love smell of napalm in the morning...");
+		System.out.println("Apocalypse Now");
+		System.out.println("Robert Duvall");
+		System.out.println("1979");
+
+	}
+
+}
