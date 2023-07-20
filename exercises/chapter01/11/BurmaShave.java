95ed66bc3a9f675281f03fb1b10cf640b34cd81e
Jakov Milas <jakov.milas@live.com>
2023-07-20 23:53:01 +0200

    Chapter 01, exercise 11

---
+++ exercises/chapter01/11/BurmaShave.java
@@ -0,0 +1,13 @@
+public class BurmaShave {
+
+	public static void main(String[] args) {
+
+		System.out.println("Shaving brushes");
+		System.out.println("You'll soon see 'em");
+		System.out.println("On a shelf");
+		System.out.println("In some museum");
+		System.out.println("Burma Shave");
+
+	}
+
+}
