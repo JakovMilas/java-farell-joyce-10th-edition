ce1bd0e212a15532460d7d073e4a368580dcafc1
Jakov Milas <jakov.milas@live.com>
2023-07-24 06:35:56 +0200

    Chapter 01, exercise 08

--- exercises/chapter01/08/TableAndChairs.java
+++ exercises/chapter01/08/TableAndChairs.java
@@ -1,3 +1,18 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 01, exercise 08
+
+Task:
+Write, compile, and test a class that displays the pattern shown in
+Figure 1-26. Save the class as TableAndChairs.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 public class TableAndChairs {
 
 	public static void main(String[] args) {
