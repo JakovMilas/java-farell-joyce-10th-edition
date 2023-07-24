a3e83ad2240c3e5e810132370fcfa41404c7066b
Jakov Milas <jakov.milas@live.com>
2023-07-24 07:19:33 +0200

    Chapter 02, exercise 5b

--- exercises/chapter02/05/MileConversion.java
+++ exercises/chapter02/05/MileConversion.java
@@ -1,3 +1,20 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 02, exercise 05a
+
+Task:
+Write a program that declares named constants to represent the number of inches, feet, and yards in
+a mile. Also declare a variable named miles to represent a number of miles and assign a value to it.
+Compute and display, with explanatory text, the value in inches, feet, and yards. Save the program as
+MileConversions.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 public class MileConversion {
 
 	public static void main(String[] args) {
