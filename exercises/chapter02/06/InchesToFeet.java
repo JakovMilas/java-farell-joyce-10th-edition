0d0929b774a973dd7f5653dd4d13c6296c8a96cc
Jakov Milas <jakov.milas@live.com>
2023-07-24 07:22:03 +0200

    Chapter 02, exercise 06a

--- exercises/chapter02/06/InchesToFeet.java
+++ exercises/chapter02/06/InchesToFeet.java
@@ -1,3 +1,19 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 02, exercise 06a
+
+Task:
+Write a program that declares a variable named inches, which holds a length in inches, and assign a
+value. Display the value in feet and inches; for example, 86 inches becomes 7 feet and 2 inches. Be sure to
+use a named constant where appropriate. Save the program as InchesToFeet.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 public class InchesToFeet {
 
 	public static void main(String[] args) {
