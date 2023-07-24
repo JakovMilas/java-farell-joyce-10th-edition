1d1ec3cae08c3399234f64f4fd49eeb5f742afcb
Jakov Milas <jakov.milas@live.com>
2023-07-24 06:53:14 +0200

    Chapter 01, Case Problems 1a

--- case-problems/yummy-catering/chapter01/YummyMotto.java
+++ case-problems/yummy-catering/chapter01/YummyMotto.java
@@ -1,3 +1,22 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 01, Case Problems 1a
+
+Task:
+The case problems in this section introduce two fictional businesses. Throughout this course, you will create
+increasingly complex classes for these businesses that use the newest concepts you have mastered in each
+chapter.
+
+Yummy Catering provides meals for parties and special events. Write a program that displays Yummy
+Catering’s motto, which is Yummy makes the food that makes it a party. Save the file as YummyMotto.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 public class YummyMotto {
 
 	public static void main(String[] args) {
