e37a5918f44e85cf6cabcdb6c5b2660e2a431086
Jakov Milas <jakov.milas@live.com>
2023-07-24 07:24:25 +0200

    Chapter 02, exercise 07

--- exercises/chapter02/07/Eggs.java
+++ exercises/chapter02/07/Eggs.java
@@ -1,3 +1,21 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 02, exercise 07
+
+Task:
+Meadowdale Dairy Farm sells organic brown eggs to local customers. It charges $3.25 for a dozen eggs or 45
+cents for individual eggs that are not part of a dozen. Write a program that prompts a user for the number of
+eggs in the order and then display the amount owed with a full explanation. For example, typical output might
+be You ordered 27 eggs. That’s 2 dozen at $3.25 per dozen and 3 loose eggs at 45 cents each
+for a total of $7.85. Save the program as Eggs.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 import java.util.Scanner;
 
 public class Eggs {
