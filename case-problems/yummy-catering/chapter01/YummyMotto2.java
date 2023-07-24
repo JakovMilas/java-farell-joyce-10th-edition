a1c736d286100793df2718303de7b9b52dd332ed
Jakov Milas <jakov.milas@live.com>
2023-07-24 06:55:19 +0200

    Chapter 01, Case Problems 1b

--- case-problems/yummy-catering/chapter01/YummyMotto2.java
+++ case-problems/yummy-catering/chapter01/YummyMotto2.java
@@ -1,3 +1,22 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 01, Case Problems 1b
+
+Task:
+The case problems in this section introduce two fictional businesses.
+Throughout this course, you will create increasingly complex classes for
+these businesses that use the newest concepts you have mastered in each chapter.
+
+Create a second program that displays the motto surrounded by a border composed of asterisks.
+Save the file as YummyMotto2.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 public class YummyMotto2 {
 
 	public static void main(String[] args) {
