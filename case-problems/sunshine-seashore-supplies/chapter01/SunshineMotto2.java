431b1763a46b6c162fa0adeebb56f2c9dc865908
Jakov Milas <jakov.milas@live.com>
2023-07-24 06:59:56 +0200

    Chapter 01, Case Problems 2b

--- case-problems/sunshine-seashore-supplies/chapter01/SunshineMotto2.java
+++ case-problems/sunshine-seashore-supplies/chapter01/SunshineMotto2.java
@@ -1,3 +1,22 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 01, Case Problems 2b
+
+Task:
+The case problems in this section introduce two fictional businesses.
+Throughout this course, you will create increasingly complex classes for
+these businesses that use the newest concepts you have mastered in each chapter.
+
+Create a second program that displays the motto surrounded by a border composed of repeated Ss.
+Save the file as SunshineMotto2.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 public class SunshineMotto2 {
 
 	public static void main(String[] args) {
