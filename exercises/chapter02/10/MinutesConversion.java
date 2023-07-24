75f5a19bd2fa0ab39e98ea31611ad600da886bdf
Jakov Milas <jakov.milas@live.com>
2023-07-24 07:29:27 +0200

    Chapter 02, exercise 10

--- exercises/chapter02/10/MinutesConversion.java
+++ exercises/chapter02/10/MinutesConversion.java
@@ -1,3 +1,19 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 02, exercise 10
+
+Task:
+Write a program that accepts an integer number of minutes from a user and converts it both to hours
+and days. For example, 6,000 minutes equals 100 hours and equals 4.167 days. Save the program as
+MinutesConversion.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 import java.util.Scanner;
 
 public class MinutesConversion {
