d85b2ea1eb1d733228858ab0f85016ba9053f4c7
Jakov Milas <jakov.milas@live.com>
2023-07-24 07:28:15 +0200

    Chapter 02, exercise 09

--- exercises/chapter02/09/Dollars.java
+++ exercises/chapter02/09/Dollars.java
@@ -1,3 +1,18 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 02, exercise 09
+
+Task:
+Write a program that allows the user to enter an integer number of dollars and calculates and displays the
+conversion into currency denominations—20s, 10s, 5s, and 1s. Save the program as Dollars.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 import java.util.Scanner;
 
 public class Dollars {
