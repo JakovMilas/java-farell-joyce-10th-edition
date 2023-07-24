e3a9acdd419691a21cd31570271e2e75c22901a4
Jakov Milas <jakov.milas@live.com>
2023-07-24 07:25:30 +0200

    Chapter 02, exercise 08a

--- exercises/chapter02/08/ChiliToGo.java
+++ exercises/chapter02/08/ChiliToGo.java
@@ -1,3 +1,20 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 02, exercise 08a
+
+Task:
+The Huntington Boys and Girls Club is conducting a fundraiser by selling chili dinners to go. The price
+is $7 for an adult’s meal and $4 for a child’s meal. Write a program that accepts the number of each type
+of meal ordered, and display the total money collected for adults’ meals, children’s meals, and all meals.
+Save the program as ChiliToGo.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 import java.util.Scanner;
 
 
