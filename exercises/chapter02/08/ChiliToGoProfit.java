f46613f78c7b51bfe3e9cdd3c25ba014aafae1fe
Jakov Milas <jakov.milas@live.com>
2023-07-24 07:26:47 +0200

    Chapter 02, exercise 08b

--- exercises/chapter02/08/ChiliToGoProfit.java
+++ exercises/chapter02/08/ChiliToGoProfit.java
@@ -1,3 +1,19 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 02, exercise 08b
+
+Task:
+In the ChiliToGo program, the costs to produce an adult’s meal and a child’s meal are $4.35 and $3.10,
+respectively. Modify the ChiliToGo program to display the total profit for each type of meal as well as
+the grand total profit. Save the program as ChiliToGoProfit.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 import java.util.Scanner;
 
 public class ChiliToGoProfit {
