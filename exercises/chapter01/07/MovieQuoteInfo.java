b02446ca8270423c88bf962c71fa4f2e61b4fb8e
Jakov Milas <jakov.milas@live.com>
2023-07-24 06:28:19 +0200

    Chapter 01, exercise 07

--- exercises/chapter01/07/MovieQuoteInfo.java
+++ exercises/chapter01/07/MovieQuoteInfo.java
@@ -1,3 +1,19 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 01, exercise 07
+
+Task:
+Write, compile, and test a class that uses four println() statements to display,
+in order, your favorite movie quote, the movie it comes from, the character who
+said it, and the year of the movie. Save the class as MovieQuoteInfo.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 public class MovieQuoteInfo {
 
 	public static void main(String[] args) {
