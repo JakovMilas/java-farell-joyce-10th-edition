fcc0de218c78fc101dc23d0667e00feb89e03e8d
Jakov Milas <jakov.milas@live.com>
2023-07-24 06:24:01 +0200

    Chapter 01, exercise 06

--- exercises/chapter01/06/SongLirics.java
+++ exercises/chapter01/06/SongLirics.java
@@ -1,3 +1,18 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 01, exercise 06
+
+Task:
+Write, compile, and test a class that uses four println() statements to display
+four lines of the lyrics of your favorite song. Save the class as SongLyrics.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 public class SongLirics {
 
 	public static void main(String[] args) {
