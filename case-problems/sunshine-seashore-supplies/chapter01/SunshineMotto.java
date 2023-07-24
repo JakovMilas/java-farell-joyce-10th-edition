0849f77945862ed0ff1e846eb43b0d14eff695ba
Jakov Milas <jakov.milas@live.com>
2023-07-24 06:57:48 +0200

    Chapter 01, Case Problems 2a

--- case-problems/sunshine-seashore-supplies/chapter01/SunshineMotto.java
+++ case-problems/sunshine-seashore-supplies/chapter01/SunshineMotto.java
@@ -1,3 +1,23 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 01, Case Problems 2a
+
+Task:
+The case problems in this section introduce two fictional businesses.
+Throughout this course, you will create increasingly complex classes for
+these businesses that use the newest concepts you have mastered in each chapter.
+
+Sunshine Seashore Supplies rents beach equipment such as kayaks, canoes, beach chairs, and umbrellas
+to tourists. Write a program that displays Sunshine’s motto, which is Sunshine Seashore makes it fun in
+the sun. Save the file as SunshineMotto.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 public class SunshineMotto {
 
 	public static void main(String[] args) {
