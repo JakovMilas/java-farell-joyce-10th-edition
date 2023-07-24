fc226e4ac39723d325419e0c36652d441724034d
Jakov Milas <jakov.milas@live.com>
2023-07-24 07:14:17 +0200

    Chapter 02, exercise 04a

--- exercises/chapter02/04/QuartsToGallons.java
+++ exercises/chapter02/04/QuartsToGallons.java
@@ -1,3 +1,21 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 02, exercise 04a
+
+Task:
+Write a program that declares a named constant to hold the number of quarts in a gallon (4). Also
+declare a variable to represent the number of quarts needed for a painting job. Name the variable
+quartsNeeded, and assign 18 to it. Compute and display the number of gallons and quarts needed for
+the job. Display explanatory text in the format A job that needs 18 quarts requires 4 gallons plus
+2 quarts. Save the program as QuartsToGallons.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 public class QuartsToGallons {
 
 	public static void main(String[] args) {
