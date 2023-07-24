295212af7a406eb307c85ec58ed33e2ff6876b3c
Jakov Milas <jakov.milas@live.com>
2023-07-24 06:43:31 +0200

    Chapter 01, exercise 11

--- exercises/chapter01/11/BurmaShave.java
+++ exercises/chapter01/11/BurmaShave.java
@@ -1,3 +1,27 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 01, exercise 11
+
+Task:
+From 1925 through 1963, Burma Shave advertising signs appeared next to highways across the United States.
+There were always four or five signs in a row containing pieces of a rhyme, followed by a final sign that read
+Burma Shave. For example, one set of signs that has been preserved by the Smithsonian Institution reads as
+follows:
+Shaving brushes
+You’ll soon see ’em
+On a shelf
+In some museum
+Burma Shave
+Find a classic Burma Shave rhyme on the Web, making sure the fifth line is Burma Shave. Write, compile, and
+test a class that displays the slogan. Save the class as BurmaShave.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 public class BurmaShave {
 
 	public static void main(String[] args) {
