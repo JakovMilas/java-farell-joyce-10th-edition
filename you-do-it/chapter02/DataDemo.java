f0341abeeec75d57dff93252b1c684713386cecf
Jakov Milas <jakov.milas@live.com>
2023-07-25 18:41:48 +0200

    Chapter 02, You Do It

---
+++ you-do-it/chapter02/DataDemo.java
@@ -0,0 +1,12 @@
+public class DataDemo {
+
+	public static void main(String[] args) {
+
+		int aWholeNumber = 315;
+
+		System.out.print("The number is ");
+		System.out.println(aWholeNumber);
+
+	}
+
+}
