9b9392a821d16cb36c5d76a6e2753fe29a0eaea9
Jakov Milas <jakov.milas@live.com>
2023-07-27 00:24:19 +0200

    Chapter 03, exercise 04

--- exercises/chapter03/Percentages.java
+++ exercises/chapter03/Percentages.java
@@ -1,3 +1,21 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 03, exercise 04
+
+Task:
+Create an application named Percentages whose main() method holds two double variables, and prompt
+the user for values. Pass both variables to a method named computePercent() that displays the two values
+and the value of the first number as a percentage of the second one. For example, if the numbers are 2.0 and 5.0,
+the method should display a statement similar to 2.0 is 40 percent of 5.0. Then call the method a second time,
+passing the values in reverse order. Save the application as Percentages.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+
 import java.util.Scanner;
 
 public class Percentages {
