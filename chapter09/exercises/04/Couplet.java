f0d5ea82a6697364882b69199e7668d5e6e540c9
Jakov <jakov.milas@live.com>
2023-08-28 03:26:44 +0200

    Chapter 09, exericse 3b

---
+++ chapter09/exercises/04/Couplet.java
@@ -0,0 +1,23 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 09, exercise 4b
+
+Task:
+Create three subclasses: Couplet, Limerick, and Haiku. The constructor for each subclass requires
+only a title; the lines field is set using a constant value. A couplet has two lines, a limerick
+has five lines, and a haiku has three lines.
+Save the files as Couplet.java, Limerick.java, and Haiku.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+public class Couplet extends Poem
+{
+	public Couplet(String name)
+	{
+		super(name, 2);
+	}
+}
