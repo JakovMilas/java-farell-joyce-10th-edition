8e1373f22e259e4501008cd12d53b4d265b859b1
Jakov <jakov.milas@live.com>
2023-09-07 12:44:25 +0200

    Chapter 09, exercise 8b

---
+++ chapter09/exercises/08/NonFiction.java
@@ -0,0 +1,30 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 09, exercise 8b
+
+Task:
+Create two child classes of Book: Fiction and NonFiction. Each must include a setPrice()
+method that sets the price for all Fiction Books to $24.99 and for all NonFiction Books to $37.99.
+Write a constructor for each subclass, and include a call to setPrice() within each. Save the files as
+Fiction.java and NonFiction.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+public class NonFiction extends Book
+{
+	public NonFiction(String title)
+	{
+		super(title);
+		setPrice();
+	}
+	@Override
+	public void setPrice()
+	{
+		price = 37.99;
+	}
+}
+
