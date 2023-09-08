5f50a5f387d9a99da6a26701bdf0a8d12b0a3993
Jakov <jakov.milas@live.com>
2023-09-08 10:00:14 +0200

    Chapter 09, exercise 13d

---
+++ chapter09/exercises/13/SidedObject.java
@@ -0,0 +1,19 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 09, exercise 13d
+
+Task:
+Create an interface named SidedObject that contains a method called displaySides(); this
+method displays a String that explains the number of sides an object possesses.
+Save the file as SidedObject.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+public interface SidedObject
+{
+	public abstract String displaySides();
+}
