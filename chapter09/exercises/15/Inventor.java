75053e5d17ed1d47846e3d38e194b75f33c8174d
Jakov <jakov.milas@live.com>
2023-09-08 11:25:34 +0200

    Chapter 09, exercise 15a

---
+++ chapter09/exercises/15/Inventor.java
@@ -0,0 +1,16 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 09, exercise 15a
+
+Task:
+Create a record named Inventor that stores two Strings: the name of an inventor
+and the inventor's country of origin. Save the file as Inventor.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+record Inventor(String name, String country)
+{
+}
