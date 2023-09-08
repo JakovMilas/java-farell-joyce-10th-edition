d513ddb8eae4a39430ed1f2e7763a3ccad0ded8a
Jakov <jakov.milas@live.com>
2023-09-08 10:29:14 +0200

    Chapter 09, exercise 14a

---
+++ chapter09/exercises/14/Runner.java
@@ -0,0 +1,17 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 09, exercise 14a
+
+Task:
+Create an interface called Runner. The interface has an abstract method called run().
+Save the file as Runner.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+public interface Runner
+{
+	public abstract String run();
+}
