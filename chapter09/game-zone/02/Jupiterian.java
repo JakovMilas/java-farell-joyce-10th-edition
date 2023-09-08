55f04b0b2f404b84d97fea74a33f4ee7c98574ca
Jakov <jakov.milas@live.com>
2023-09-08 12:53:52 +0200

    Chapter 09, Game Zone 2b

---
+++ chapter09/game-zone/02/Jupiterian.java
@@ -0,0 +1,25 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 09, Game Zone 2b
+
+Task:
+Create two classes?Martian and Jupiterian?that descend from Alien. Supply each with a
+constructor that sets the Alien data fields with values you choose. For example, you can decide that a
+Martian has four eyes but a Jupiterian has only two. Save the files as Martian.java and Jupiterian.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+public class Jupiterian extends Alien
+{
+	public Jupiterian()
+	{
+		numberOfEyes = 2;
+		skinType = "slime";
+		skinColor = "orange";
+		origin = "Jupiter";
+	}
+}
