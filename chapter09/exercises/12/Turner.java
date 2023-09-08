d0245900d036d45899b799171a4bb34da90a6696
Jakov <jakov.milas@live.com>
2023-09-08 07:09:24 +0200

    Chapter 09, exercise 12a

---
+++ chapter09/exercises/12/Turner.java
@@ -0,0 +1,16 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 09, exercise 12a
+
+Task:
+Create an interface named Turner, with a single method named turn(). Save the file as Turner.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+public interface Turner
+{
+	public abstract void turn();
+}
