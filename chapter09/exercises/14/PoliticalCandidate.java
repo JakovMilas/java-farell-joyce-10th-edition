3b89f83bef915d43ac1227838e0fd3d124ddadbd
Jakov <jakov.milas@live.com>
2023-09-08 10:29:32 +0200

    Chapter 09, exercise 14b

---
+++ chapter09/exercises/14/PoliticalCandidate.java
@@ -0,0 +1,23 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 09, exercise 14b
+
+Task:
+Create classes called Machine, Athlete, and PoliticalCandidate that all implement Runner.
+The run() method displays a message describing the meaning of run to each class. Save the files as
+Machine.java, Athlete.java, and PoliticalCandidate.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+public class PoliticalCandidate implements Runner
+{
+	@Override
+	public String run()
+	{
+		return "Political candidate is running.";
+	}
+}
