745b5704e77dca23ac23e44b8de0786c12a608e6
Jakov <jakov.milas@live.com>
2023-09-08 07:09:55 +0200

    Chapter 09, exercise 12b

---
+++ chapter09/exercises/12/Pancake.java
@@ -0,0 +1,24 @@
+/*
+Farrel Joyce - Java Programming, 10th Edition (2023)
+
+Chapter 09, exercise 12b
+
+Task:
+ Create a class named Leaf that implements turn() to display Changing colors. Create a class
+named Page that implements turn() to display Going to the next page. Create a class named
+Pancake that implements turn() to display Flipping. Save the files as Leaf.java, Page.java, and
+Pancake.java.
+
+Written by: Jakov Milas
+https://github.com/JakovMilas/java-farell-joyce-10th-edition
+*/
+
+
+public class Pancake implements Turner
+{
+	@Override
+	public void turn()
+	{
+		System.out.println("Flipping.");
+	}
+}
