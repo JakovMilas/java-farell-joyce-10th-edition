a8432dd4d3aa08dedd124129797eaf3e3f9299e6
Jakov Milas <jakov.milas@live.com>
2023-11-09 03:20:17 +0100

    Chapter 11, exercise 4c

--- chapter11/exercises/04/DisplaySavedCustomerList.java
+++ chapter11/exercises/04/DisplaySavedCustomerList.java
@@ -1,7 +1,7 @@
 /*
 Farrel Joyce - Java Programming, 10th Edition (2023)
 
-Chapter 11, exercise 4b
+Chapter 11, exercise 4c
 
 Task:
 Write an application that reads the file created by the WriteCustomerList application and displays
